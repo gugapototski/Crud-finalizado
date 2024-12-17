4706fdae4a33faa211b0fdb81d31c90eca239b78
Luiz <gugapototski@live.com>
2024-12-17 20:29:47 -0300

    first commit

---
+++ ProdutoAPI/ProdutoAPI/src/main/java/Produto/ProdutoAPI/controllers/ProdutoController.java
@@ -0,0 +1,45 @@
+package Produto.ProdutoAPI.controllers;
+
+import Produto.ProdutoAPI.models.Produto;
+import Produto.ProdutoAPI.services.ProdutoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @GetMapping
+    public List<Produto> listarProdutos() {
+        return produtoService.listarProdutos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> buscarProdutoPorId(@PathVariable Long id) {
+        Produto produto = produtoService.buscarProdutoPorId(id);
+        return produto != null ? ResponseEntity.ok(produto) : ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public Produto criarProduto(@RequestBody Produto produto) {
+        return produtoService.criarProduto(produto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Produto> atualizarProduto(@PathVariable Long id, @RequestBody Produto produtoAtualizado) {
+        Produto produto = produtoService.atualizarProduto(id, produtoAtualizado);
+        return produto != null ? ResponseEntity.ok(produto) : ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarProduto(@PathVariable Long id) {
+        produtoService.deletarProduto(id);
+        return ResponseEntity.noContent().build();
+    }
+}
